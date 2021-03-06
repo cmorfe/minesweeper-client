ade812ee92fc3631b7ee0dce4b09f300689d768c
Carlos Morfe <cmorfe@alquilando.com>
2021-03-05 22:48:24 -0300

    env test

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
       <div className="app">
         <div className="navbar">
           <div>
-            <Typography variant="h6">{process.env.REACT_APP_API_URL}</Typography>
+            <Typography variant="h6">{process.env.NODE_ENV}</Typography>
           </div>
           <div>
             <Link
